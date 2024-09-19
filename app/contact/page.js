9283d3f35137e7d7a2471596e2c819cc281f8f8b
Umar Ejaz <johndoe@example.com~>
2024-09-19 12:17:48 -0700

    final

--- app/contact/page.js
+++ app/contact/page.js
@@ -3,7 +3,7 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-      <h1>Hello</h1>
+      <h1>contact</h1>
     </div>
   )
 }
--- app/home/page.js
+++ app/home/page.js
@@ -3,7 +3,7 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-      <h1>Hello</h1>
+      <h1>home</h1>
     </div>
   )
 }
